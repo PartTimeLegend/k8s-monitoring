849fc33b1f1d7aa261de7faca1fbaea431016886
Antony Bailey <antony_bailey@waters.com>
2022-09-01 15:07:19 +0100

    Add Jaeger

---
+++ 01-jaeger/providers.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.aws_region
+  assume_role {
+    role_arn = var.assume_role_arn
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = ">= 1.0"
+  #backend "s3" {
+  #}
+}
+
+provider "kubernetes" {
+  host                   = var.cluster_name
+  cluster_ca_certificate = base64decode(var.cluster_ca)
+  token                  = var.cluster_auth
+}
---
+++ 01-jaeger/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "assume_role_arn" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_ca" {
+  type = string
+}
+
+variable "cluster_auth" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
